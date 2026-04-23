5b1f8162cdb7dc599f23a433d476c9c8d6becee4
GabriellCabrall <gabrielbielcabral@gmail.com>
2026-04-23 08:55:44 -0300

    Corrige confiuração do alembic

--- alembic/env.py
+++ alembic/env.py
@@ -28,6 +28,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.db.session import Base
+import app.models
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
