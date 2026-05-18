9714d7c25e950bd3aceca286cd9d23397814e2b6
GabriellCabrall <gabrielbielcabral@gmail.com>
2026-05-18 10:53:38 -0300

    Cria endpoint de listar especies e arruma o script de popular o banco

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import planta
 from app.core.config import settings
 from app.routers.health import router as health_router
 from app.routers.db_test import router as db_test_router
 from app.routers.usuarios import router as usuarios_router
+from app.routers.especie import router as especie_router
 
 app = FastAPI(title=settings.app_name)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8081"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(health_router)
 app.include_router(db_test_router)
 app.include_router(usuarios_router)
 app.include_router(planta.router)
+app.include_router(especie_router)
 
 @app.get("/")
 def root():
